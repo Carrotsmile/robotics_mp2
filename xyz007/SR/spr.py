a20e2b99d9e9de57040572edbe1f1497e01cec68
scarletpawn <oa153@scarletmail.rutgers.edu>
2017-11-21 18:29:09 -0500

    first commit

---
+++ xyz007/SR/spr.py
@@ -0,0 +1,158 @@
+import sys
+import numpy as np
+from math import sqrt
+'''
+Report reflexive vertices
+'''
+def findReflexiveVertices(polygons):
+    vertices=[]
+    
+    # Your code goes here
+    # You should return a list of (x,y) values as lists, i.e.
+    # vertices = [[x1,y1],[x2,y2],...]
+    for polygon in polygons:
+        for i in range(len(polygon)):
+            left = np.array(polygon[i-1])
+            mid = np.array(polygon[i])
+            right = np.array(polygon[(i + 1) % len(polygon)])
+
+            det = np.linalg.det(np.array([left-mid, right-mid]))
+            if det > 0:
+                vertices.append(polygon[i])
+    return vertices
+
+'''
+Euclidean Distance between vertices
+'''
+def distance(v1, v2):
+    a = (v1[0] - v2[0]) ** 2
+    b = (v1[1] - v2[1]) ** 2
+    return sqrt(a + b)
+'''
+Compute the roadmap graph
+'''
+def computeSPRoadmap(polygons, reflexVertices):
+
+    vertexMap = dict()
+    reflexMap = dict()
+    adjacencyListMap = dict()
+    
+    for i in range(1, len(reflexVertices) + 1):
+        vertexMap[i] = reflexVertices[i-1]
+        reflexMap[reflexVertices[i-1]] = reflexVertices[i-1] 
+
+    for polygon in polygons:
+        for i in range(len(polygon)):
+            left = reflexMap[polygon[i - 1]]
+            right = reflexMap[polygon[(i+1) % len(polygon)]]
+            if left != None:
+                #add left to adjacencyListMap
+
+
+    # Your code goes here
+    # You should check for each pair of vertices whether the
+    # edge between them should belong to the shortest path
+    # roadmap. 
+    #
+    # Your vertexMap should look like
+    # {1: [5.2,6.7], 2: [9.2,2.3], ... }
+    #
+    # and your adjacencyListMap should look like
+    # {1: [[2, 5.95], [3, 4.72]], 2: [[1, 5.95], [5,3.52]], ... }
+    #
+    # The vertex labels used here should start from 1
+    
+    return vertexMap, adjacencyListMap
+
+'''
+Perform uniform cost search 
+'''
+def uniformCostSearch(adjListMap, start, goal):
+    path = []
+    pathLength = 0
+    
+    # Your code goes here. As the result, the function should
+    # return a list of vertex labels, e.g.
+    #
+    # path = [23, 15, 9, ..., 37]
+    #
+    # in which 23 would be the label for the start and 37 the
+    # label for the goal.
+    
+    return path, pathLength
+
+'''
+Agument roadmap to include start and goal
+'''
+def updateRoadmap(polygons, vertexMap, adjListMap, x1, y1, x2, y2):
+    updatedALMap = dict()
+    startLabel = 0
+    goalLabel = -1
+
+    # Your code goes here. Note that for convenience, we 
+    # let start and goal have vertex labels 0 and -1,
+    # respectively. Make sure you use these as your labels
+    # for the start and goal vertices in the shortest path
+    # roadmap. Note that what you do here is similar to
+    # when you construct the roadmap. 
+    
+    return startLabel, goalLabel, updatedALMap
+
+if __name__ == "__main__":
+    
+    # Retrive file name for input data
+    if(len(sys.argv) < 6):
+        print "Five arguments required: python spr.py [env-file] [x1] [y1] [x2] [y2]"
+        exit()
+    
+    filename = sys.argv[1]
+    x1 = float(sys.argv[2])
+    y1 = float(sys.argv[3])
+    x2 = float(sys.argv[4])
+    y2 = float(sys.argv[5])
+
+    # Read data and parse polygons
+    lines = [line.rstrip('\n') for line in open(filename)]
+    polygons = []
+    for line in range(0, len(lines)):
+        xys = lines[line].split(';')
+        polygon = []
+        for p in range(0, len(xys)):
+            polygon.append(map(float, xys[p].split(',')))
+        polygons.append(polygon)
+
+    # Print out the data
+    print "Pologonal obstacles:"
+    for p in range(0, len(polygons)):
+        print str(polygons[p])
+    print ""
+
+    # Compute reflex vertices
+    reflexVertices = findReflexiveVertices(polygons)
+    print "Reflexive vertices:"
+    print str(reflexVertices)
+    print ""
+
+    # Compute the roadmap 
+    vertexMap, adjListMap = computeSPRoadmap(polygons, reflexVertices)
+    print "Vertex map:"
+    print str(vertexMap)
+    print ""
+    print "Base roadmap:"
+    print str(adjListMap)
+    print ""
+
+    # Update roadmap
+    start, goal, updatedALMap = updateRoadmap(polygons, vertexMap, adjListMap, x1, y1, x2, y2)
+    print "Updated roadmap:"
+    print str(updatedALMap)
+    print ""
+
+    # Search for a solution     
+    path, length = uniformCostSearch(updatedALMap, start, goal)
+    print "Final path:"
+    print str(path)
+    print "Final path length:" + str(length)
+    
+
+    # Extra visualization elements goes here
